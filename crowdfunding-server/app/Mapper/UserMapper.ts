86474e2bcc9a546f948666ca7425d974946baa0d
akwanmaroso <akwanmaroso@gmail.com>
2023-03-04 18:33:15 +0800

    feat: add field id in mapper user

--- crowdfunding-server/app/Mapper/UserMapper.ts
+++ crowdfunding-server/app/Mapper/UserMapper.ts
@@ -3,6 +3,7 @@ import User from "App/Models/User";
 export default class UserMapper {
   public static map(user: User) {
     return {
+      id: user.id,
       fullname: user.fullname,
       email: user.email,
     };
