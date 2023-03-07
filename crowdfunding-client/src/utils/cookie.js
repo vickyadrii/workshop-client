77f8a3b2f08cf9d02dfdfcd0e04ceb3ad39f4d12
akwanmaroso <akwanmaroso@gmail.com>
2023-03-07 20:43:03 +0800

    remove unused lib jwtcode

--- crowdfunding-client/src/utils/cookie.js
+++ crowdfunding-client/src/utils/cookie.js
@@ -1,12 +1,8 @@
 import Cookie from "js-cookie";
-import jwtDecode from "jwt-decode";
-import { milisecondsToDays } from "./date";
 
 export const CROWDFUNDING_COOKIE = "CROWDFUNDING_COOKIE";
 
 export const setAccessTokenCookie = (accessToken) => {
-  const decodedToken = accessToken;
-
   Cookie.set(CROWDFUNDING_COOKIE, accessToken);
 };
 
